d5125671aa17df18fd80f018ab7121d56928fa84
VardgesM <v.mirzakhanyann@gmail.com>
2023-07-30 01:10:22 +0400

    add subscribe to signature

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,17 +7,25 @@ export class AppController {
 
   @Post('play')
   async play() {
-    await this.appService.subscribeToAccountChange();
+    // await this.appService.subscribeToAccountChange();
     await this.watchBalance();
   }
 
   @Post('add-balance')
   async addBalance() {
-    await this.appService.addAccountBalance();
+    await this.appService.addAccountBalance(process.env.WALLET_ADDRESS);
   }
 
   @Post('watch-balance')
   async watchBalance() {
-    return await this.appService.watchAccountBalance();
+    return await this.appService.watchAccountBalance(
+      process.env.WALLET_ADDRESS,
+    );
+  }
+
+  @Post('send-transaction')
+  async sendTransaction() {
+    const sendResult = await this.appService.sendTransaction();
+    await this.appService.subscribeToSignature(sendResult.signature);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import {
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  Keypair,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
 
 @Injectable()
 export class AppService {
   private solanaConnection: Connection;
 
   constructor() {
-    const WSS_ENDPOINT = process.env.WSS_ENDPOINT;
-    const HTTP_ENDPOINT = process.env.HTTP_ENDPOINT;
+    // const WSS_ENDPOINT = process.env.WSS_ENDPOINT;
+    // const HTTP_ENDPOINT = process.env.HTTP_ENDPOINT;
 
     // const WSS_ENDPOINT = 'wss://testnet.solana.com';
     // const HTTP_ENDPOINT = 'https://api.testnet.solana.com';
 
-    this.solanaConnection = new Connection(HTTP_ENDPOINT, {
-      wsEndpoint: WSS_ENDPOINT,
-    });
+    this.solanaConnection = new Connection(
+      'https://api.testnet.solana.com',
+      'confirmed',
+    );
   }
 
-  async watchAccountBalance(): Promise<any> {
-    const ACCOUNT_TO_WATCH = new PublicKey(process.env.WALLET_ADDRESS);
-
+  async watchAccountBalance(walletAddress: any): Promise<any> {
+    const ACCOUNT_TO_WATCH = new PublicKey(walletAddress);
     const balance = await this.solanaConnection.getBalance(ACCOUNT_TO_WATCH);
     console.log(`Wallet Balance: ${balance / LAMPORTS_PER_SOL}`);
     return { balance: balance / LAMPORTS_PER_SOL };
   }
 
-  async addAccountBalance(): Promise<any> {
-    const ACCOUNT_TO_WATCH = new PublicKey(process.env.WALLET_ADDRESS);
+  async addAccountBalance(walletAddress: any): Promise<any> {
+    const ACCOUNT_TO_WATCH = new PublicKey(walletAddress);
 
     await this.solanaConnection.requestAirdrop(
       ACCOUNT_TO_WATCH,
@@ -36,8 +44,8 @@ export class AppService {
     console.log('Balance Added');
   }
 
-  async subscribeToAccountChange(): Promise<any> {
-    const ACCOUNT_TO_WATCH = new PublicKey(process.env.WALLET_ADDRESS);
+  async subscribeToAccountChange(walletAddress: any): Promise<any> {
+    const ACCOUNT_TO_WATCH = new PublicKey(walletAddress);
     const subscriptionId = await this.solanaConnection.onAccountChange(
       ACCOUNT_TO_WATCH,
       (updatedAccountInfo) => {
@@ -53,7 +61,63 @@ export class AppService {
     console.log('Starting web socket, subscription ID: ', subscriptionId);
   }
 
-  // private sleep(ms: number): Promise<void> {
-  //   return new Promise((resolve) => setTimeout(resolve, ms));
-  // }
+  async sendTransaction(): Promise<any> {
+    const fromKeypair = Keypair.generate();
+    const toKeypair = Keypair.generate();
+
+    const airdropSignature = await this.solanaConnection.requestAirdrop(
+      fromKeypair.publicKey,
+      LAMPORTS_PER_SOL,
+    );
+
+    await this.solanaConnection.confirmTransaction(airdropSignature);
+
+    console.log(
+      (await this.solanaConnection.getBalance(fromKeypair.publicKey)) /
+        LAMPORTS_PER_SOL,
+    );
+    const lamportsToSend = 1000000000 / 2;
+
+    const transferTransaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: fromKeypair.publicKey,
+        toPubkey: toKeypair.publicKey,
+        lamports: lamportsToSend,
+      }),
+    );
+
+    const sign = await sendAndConfirmTransaction(
+      this.solanaConnection,
+      transferTransaction,
+      [fromKeypair],
+    );
+
+    return {
+      signature: sign,
+    };
+  }
+
+  async subscribeToSignature(signature: string): Promise<void> {
+    this.solanaConnection.onSignature(signature, (result) => {
+      if (result.err) {
+        console.error(
+          'Transaction failed:',
+          result.err,
+          '\n' + 'Signature:',
+          signature,
+        );
+      } else {
+        console.log(
+          'Transaction confirmed:',
+          result,
+          '\n' + 'Signature:',
+          signature,
+        );
+      }
+    });
+  }
+
+  private sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
